2e276dbc81ce60237380e086a7e62942a6f1b24b
Rollan <rollan.sapyshev@gmail.com>
2023-08-24 15:59:15 +0600

    predict

--- 4-deployment/web-service/predict.py
+++ 4-deployment/web-service/predict.py
@@ -4,4 +4,10 @@ with open("././models/svm_model.bin", "rb") as f_in:
     (dv, model) = pickle.load(f_in)
 
 def predict(features):
-    pred = model.predict(features)
+    pred = model.
+
+
+
+
+
+
